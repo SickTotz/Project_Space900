5636c5fa3aa50884d1b76c2a5779435a760f8b0e
Tiziano <dev.tizianovelardi@gmail.com>
2023-04-16 15:00:10 +0200

    1.11.1° Commit

---
+++ Space900/Assets/Scripts/Movement_Navicella.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement_Navicella : MonoBehaviour
+{   
+    // Creazione di variabili per il movimento del player
+    public float forwardSpeed = 25f, strafeSpeed = 7.5f, hoverSpeed = 5f;
+    private float activeForwardSpeed, activeStrafeSpeed, activeHoverSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Utilizzo delle variabili per i movimenti del player
+        activeForwardSpeed = Input.GetAxisRaw("Vertical") * forwardSpeed;
+        activeStrafeSpeed = Input.GetAxisRaw("Horizontal") * strafeSpeed;
+        activeHoverSpeed = Input.GetAxisRaw("Hover") * hoverSpeed;
+
+        // Attribuizione del movimento al player
+        transform.position += transfrom.forward * activeForwardSpeed * Time.deltaTime;
+    }
+}
