11d99b3cf8c1011b3c30b3e6361af722d483bf33
Tiziano <dev.tizianovelardi@gmail.com>
2023-04-16 12:37:17 +0200

    1.11.03° Commit

--- Space900/Assets/Scripts/TestVSCode.cs
+++ Space900/Assets/Scripts/TestVSCode.cs
@@ -7,7 +7,7 @@ public class TestVSCode : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+       Debug.Log(true); 
     }
 
     // Update is called once per frame
