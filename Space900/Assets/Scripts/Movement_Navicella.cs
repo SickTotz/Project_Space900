d875cc0bc2bfba3d88615902bb0f0bfd6b0b0e64
Tiziano <dev.tizianovelardi@gmail.com>
2023-04-16 17:08:42 +0200

    Minimal Update

--- Space900/Assets/Scripts/Movement_Navicella.cs
+++ Space900/Assets/Scripts/Movement_Navicella.cs
@@ -9,15 +9,38 @@ public class Movement_Navicella : MonoBehaviour
     private float activeForwardSpeed, activeStrafeSpeed, activeHoverSpeed;
     private float forwardAcceleration = 2.5f, strafeAcceleration = 2f, hoverAcceleration = 2f;
 
+    public float lookRateSpeed = 90f;
+    private Vector2 lookInput, screenCenter, mouseDistance;
+
+    private float rollInput;
+    public float rollSpeed = 90f, rollAcceleration = 3.5f;
+
     // Start is called before the first frame update
     void Start()
-    {
-        
+    {   
+        // Variabili per la definizione della visuale in gioco
+        screenCenter.x = Screen.width * .5f;
+        screenCenter.y = Screen.height * .5f;
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {   
+        // Impostazioni per la visualizzaione con il mouse
+        lookInput.x = Input.mousePosition.x;
+        lookInput.y = Input.mousePosition.y;
+
+        // Setting della velocita` massima del movimento da input col mouse
+        mouseDistance.x = (lookInput.x - screenCenter.x) / screenCenter.y;
+        mouseDistance.y = (lookInput.y - screenCenter.y) / screenCenter.y;
+
+        mouseDistance = Vector2.ClampMagnitude(mouseDistance, 1f);
+
+        rollInput = Mathf.Lerp(rollInput, Input.GetAxisRaw("Roll"), rollAcceleration * Time.deltaTime);
+
+        transform.Rotate(-mouseDistance.y * lookRateSpeed * Time.deltaTime, mouseDistance.x * lookRateSpeed * Time.deltaTime, rollInput * rollSpeed * Time.deltaTime, Space.Self);
+
+
         // Utilizzo delle variabili per i movimenti del player
         activeForwardSpeed = Mathf.Lerp(activeForwardSpeed, Input.GetAxisRaw("Vertical") * forwardSpeed, forwardAcceleration * Time.deltaTime);
         activeStrafeSpeed = Mathf.Lerp(activeStrafeSpeed, Input.GetAxisRaw("Horizontal") * strafeSpeed, strafeAcceleration * Time.deltaTime);
