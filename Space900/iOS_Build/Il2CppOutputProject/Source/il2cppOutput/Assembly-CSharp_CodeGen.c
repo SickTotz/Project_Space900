a5e1931b6667943d7c9180d6a9f6b7867ed51239
Tiziano <dev.tizianovelardi@gmail.com>
2023-04-22 13:13:45 +0200

    1.12.3° Commit
    Aggiunta di una cartella "iOS_Build" per poter builldare il gioco su iPhone.

---
+++ Space900/iOS_Build/Il2CppOutputProject/Source/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,104 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern void BeltObject_SetupBeltObject_m83E10D464225858FE506E78AE3595601E4A54DBC (void);
+extern void BeltObject_Update_m798139F69462BE4A80274B8966D5C34D708B7031 (void);
+extern void BeltObject__ctor_m8D7495B158B9317AA4BADD16456A8D2C7CF2E666 (void);
+extern void Pianeta_SpawnerAsteroidi_Start_m7868E5BE13AA8A7A4B9134C7E155C445D5EC0E87 (void);
+extern void Pianeta_SpawnerAsteroidi__ctor_m79CCADB76C75220067DE262246EBACA2496C6672 (void);
+extern void TotalSpace_SpawnerAsteroidi_Start_mE92391B6EA34411E82EF01EF1D770062969A431A (void);
+extern void TotalSpace_SpawnerAsteroidi_Update_m71C6750FFC7B0735C9E2C80238F2819E9669C0F7 (void);
+extern void TotalSpace_SpawnerAsteroidi_SpawnAsteroids_mFCE1672B7A5366DE7E130A86BE18B704B2445911 (void);
+extern void TotalSpace_SpawnerAsteroidi_MoveAsteroids_m42E5BBE59AB254472F7899E6306EE7381E427659 (void);
+extern void TotalSpace_SpawnerAsteroidi__ctor_m09DFD5CE0EAB1FDCBE0560962FE46DC54F86ACA2 (void);
+extern void Mobile_TouchControls_Start_m031363B9DD1AA2366B094677759A4F6E06E52DF8 (void);
+extern void Mobile_TouchControls_Update_m147A2562919859ECF631B91EF8F122B2E2F05082 (void);
+extern void Mobile_TouchControls__ctor_m69745D09B210E6F31B3667175248E2284926A67C (void);
+extern void MovementMechanics_Start_mD3850CC294D2FF74C23E8FFF39C51FF326AE5137 (void);
+extern void MovementMechanics_Update_m91FAB8D13CE87A5DCEEABF0DCA43F21F75982B2A (void);
+extern void MovementMechanics__ctor_mF9A1941C07B635404E3261B240FFFB5ED812C792 (void);
+extern void TestVSCode_Start_mA0BD68E6744BB3F99A0048847B12A6BFE97233BD (void);
+extern void TestVSCode_Update_mA778C9F8766D7D07A45C4C3A4E25E91C0B4A9844 (void);
+extern void TestVSCode__ctor_mE2FC78AA4137147D7ADF9C7BAF6A6D7838EAA55E (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1 (void);
+static Il2CppMethodPointer s_methodPointers[21] = 
+{
+	BeltObject_SetupBeltObject_m83E10D464225858FE506E78AE3595601E4A54DBC,
+	BeltObject_Update_m798139F69462BE4A80274B8966D5C34D708B7031,
+	BeltObject__ctor_m8D7495B158B9317AA4BADD16456A8D2C7CF2E666,
+	Pianeta_SpawnerAsteroidi_Start_m7868E5BE13AA8A7A4B9134C7E155C445D5EC0E87,
+	Pianeta_SpawnerAsteroidi__ctor_m79CCADB76C75220067DE262246EBACA2496C6672,
+	TotalSpace_SpawnerAsteroidi_Start_mE92391B6EA34411E82EF01EF1D770062969A431A,
+	TotalSpace_SpawnerAsteroidi_Update_m71C6750FFC7B0735C9E2C80238F2819E9669C0F7,
+	TotalSpace_SpawnerAsteroidi_SpawnAsteroids_mFCE1672B7A5366DE7E130A86BE18B704B2445911,
+	TotalSpace_SpawnerAsteroidi_MoveAsteroids_m42E5BBE59AB254472F7899E6306EE7381E427659,
+	TotalSpace_SpawnerAsteroidi__ctor_m09DFD5CE0EAB1FDCBE0560962FE46DC54F86ACA2,
+	Mobile_TouchControls_Start_m031363B9DD1AA2366B094677759A4F6E06E52DF8,
+	Mobile_TouchControls_Update_m147A2562919859ECF631B91EF8F122B2E2F05082,
+	Mobile_TouchControls__ctor_m69745D09B210E6F31B3667175248E2284926A67C,
+	MovementMechanics_Start_mD3850CC294D2FF74C23E8FFF39C51FF326AE5137,
+	MovementMechanics_Update_m91FAB8D13CE87A5DCEEABF0DCA43F21F75982B2A,
+	MovementMechanics__ctor_mF9A1941C07B635404E3261B240FFFB5ED812C792,
+	TestVSCode_Start_mA0BD68E6744BB3F99A0048847B12A6BFE97233BD,
+	TestVSCode_Update_mA778C9F8766D7D07A45C4C3A4E25E91C0B4A9844,
+	TestVSCode__ctor_mE2FC78AA4137147D7ADF9C7BAF6A6D7838EAA55E,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1,
+};
+static const int32_t s_InvokerIndices[21] = 
+{
+	806,
+	5233,
+	5233,
+	5233,
+	5233,
+	5233,
+	5233,
+	5233,
+	5233,
+	5233,
+	5233,
+	5233,
+	5233,
+	5233,
+	5233,
+	5233,
+	5233,
+	5233,
+	5233,
+	7824,
+	5233,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	21,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+};
