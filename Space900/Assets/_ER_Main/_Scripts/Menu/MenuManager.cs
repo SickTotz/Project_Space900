e3a833e55768a5749066f2b64956813721a24921
Tiziano <dev.tizianovelardi@gmail.com>
2023-07-18 12:55:13 +0200

    Aggiunta una animazione renderizzata su blender per il cariamento nella scena di gioco

--- Space900/Assets/_ER_Main/_Scripts/Menu/MenuManager.cs
+++ Space900/Assets/_ER_Main/_Scripts/Menu/MenuManager.cs
@@ -1,15 +1,42 @@
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
 
 public class MenuManager : MonoBehaviour
 {
+    public VideoPlayer videoPlayer;
+    public GameObject menuUI;
+    public string gameSceneName = "ER_TestScene";
+    private bool isVideoFinished = false;
+
+    private void Start()
+    {
+        videoPlayer.loopPointReached += OnVideoFinished;
+        videoPlayer.Stop();
+    }
+
     public void PlayGame()
     {
-        SceneManager.LoadScene("ER_TestScene");
+        menuUI.SetActive(false);
+        videoPlayer.Play();
     }
 
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    private void Update()
+    {
+        if (isVideoFinished && !videoPlayer.isPlaying)
+        {
+            SceneManager.LoadScene(gameSceneName);
+        }
+    }
+
+    private void OnVideoFinished(VideoPlayer vp)
+    {
+        isVideoFinished = true;
     }
 }
